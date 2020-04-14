ebb35261a7663fc2ed342aed400ff267c3826409
widberg <jaredwidberg@gmail.com>
2020-04-14 02:38:39 -0400

    Broken but want to save

--- README.md
+++ README.md
@@ -2,36 +2,44 @@
 CLI template replacement tool
 
 ### Usage
-command
+```sh
+tmpl TEMPLATE OUTPUT [ARG]...
 ```
+
+### Example
+command
+```sh
 tmpl template.txt output.txt arg0 arg1 arg2
 ```
 
 template.txt
-```
+```plain_text
 Example: $(0) $(1) $(2) $(TEMPLATE) $(OUTPUT) $(%c %Z) $$
 ```
 
 output.txt
-```
+```plain_text
 Example: arg0 arg1 arg2 template.txt output.txt 04/13/20 20:36:48 Eastern Daylight Time $
 ```
 ### Indexes
-Any replacement macro beginning with a numerical digit will be interpreted as an index. Each index is replaced by the argument passed to tmpl in that possition following the output file path. The fist argument following the output path is denoted by index 0.
+Any replacement macro beginning with a numerical digit will be interpreted as an index. Each index is replaced by the argument passed to tmpl in that possition following the output path. The fist argument following the output path is denoted by index 0.
 
 ### Token
 Any replacement macro that matches a string bellow will be replaced with the value described to the right of the string. Tokens are case sensitive.
 
 | Token | Description |
 | --- | --- |
-|TEMPLATE | The argument given as the template file path |
-| OUTPUT | The argument given as the output file path |
+|TEMPLATE | The argument given as the template path |
+| OUTPUT | The argument given as the output path |
 
 ### Time
 All replacement macros that are not an index or token will be interpreted as a time format string. Valid foramt specifiers for this use case can be found [here](http://www.cplusplus.com/reference/ctime/strftime/).
 
 ### Escape Sequences
 A $ immediately followed by another $ will be replaced by a $.
+
+### Directory Mode
+If the template path is a directory, tmpl will recursively replace the names and contents of files and subdirectories at the output path.
 
 ### Checkout
 
@@ -46,4 +54,9 @@ mkdir build
 cd build
 cmake ..
 cmake --build .
+```
+
+### Install
+```sh
+cmake --build . --config Release --target install
 ```
--- src/tmpl.cpp
+++ src/tmpl.cpp
@@ -1,11 +1,20 @@
 #include "tmpl.hpp"
 
-#include <string>
+#include <filesystem>
 #include <fstream>
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 #include <sstream>
+#include <string>
 #include <unordered_map>
+#include <vector>
+
+typedef std::unordered_map<std::string, std::string> macro_map_t;
+typedef std::vector<std::string> arg_vector_t;
+
+int replace(const std::filesystem::path& template_path, const std::filesystem::path& output_path, const std::tm* time, const macro_map_t& macros, const arg_vector_t& args);
+int replace_stream(std::istream& template_path, std::ostream& output_path, const std::tm* time, const macro_map_t& macros, const arg_vector_t& args);
+std::filesystem::path reparent_path(const std::filesystem::path& path, const std::filesystem::path& old_root, const std::filesystem::path& new_root);
 
 int main(int argc, char* argv[])
 {
@@ -15,51 +24,104 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	std::ifstream template_file(argv[1], std::ios::binary);
-	if (template_file.fail())
-	{
-		std::cerr << "Could not open template file " << argv[1] << std::endl;
-		return 1;
-	}
+	// Setup replacement values
+	const std::time_t t = std::time(nullptr);
+	const std::tm* time = std::localtime(&t);
 
-	std::ofstream output_file(argv[2], std::ios::binary);
-	if (output_file.fail())
-	{
-		std::cerr << "Could not open output file " << argv[2] << std::endl;
-		template_file.close();
-		return 1;
-	}
-
-	std::time_t t = std::time(nullptr);
-	std::tm tm = *std::localtime(&t);
-
-	std::unordered_map<std::string, std::string> macros = {
+	const macro_map_t macros = {
 		{ "TEMPLATE", argv[1] },
 		{ "OUTPUT", argv[2] }
 	};
 
+	const arg_vector_t args(argv + 3, argv + argc);
+
+	return replace(argv[1], argv[2], time, macros, args);
+}
+
+int replace(const std::filesystem::path& template_path, const std::filesystem::path& output_path, const std::tm* time, const macro_map_t& macros, const arg_vector_t& args)
+{
+	bool directory_mode = false;
+	std::ifstream template_file;
+	std::ofstream output_file;
+
+	int status = 0;
+
+	// Template file
+
+	if (std::filesystem::is_directory(template_path))
+	{
+		directory_mode = true;
+	}
+	else
+	{
+		template_file.open(template_path, std::ios::binary);
+		if (template_file.fail())
+		{
+			std::cerr << "Could not open template file " << template_path << std::endl;
+			return 1;
+		}
+	}
+
+	// replace on template path to get output path
+
+	// Output file
+
+	if (directory_mode)
+	{
+		if (std::filesystem::exists(output_path))
+		{
+			std::cerr << "Output path must not exist in directory mode" << std::endl;
+			return 1;
+		}
+	}
+	else
+	{
+		output_file.open(output_path, std::ios::binary);
+		if (output_file.fail())
+		{
+			std::cerr << "Could not open output file " << output_path << std::endl;
+			template_file.close();
+			return 1;
+		}
+	}
+
+	if (directory_mode)
+	{
+		std::filesystem::directory_iterator end_itr;
+		for (std::filesystem::directory_iterator itr(template_path); itr != end_itr; ++itr)
+		{
+			status = replace(itr->path(), reparent_path(itr->path(), template_path, output_path), time, macros, args);
+			if (status != 0)
+			{
+				break;
+			}
+		}
+	}
+	else
+	{
+		status = replace_stream(template_file, output_file, time, macros, args);
+
+		template_file.close();
+		output_file.close();
+	}
+
+	return status;
+}
+
+int replace_stream(std::istream& template_stream, std::ostream& output_stream, const std::tm* time, const macro_map_t& macros, const arg_vector_t& args)
+{
 	char c;
 
-	while (template_file.get(c) && !template_file.eof())
+	while (template_stream.get(c) && !template_stream.eof())
 	{
 		if (c == '$')
 		{
-			if (template_file.peek() == EOF)
-			{
-				std::cerr << "Incomplete macro" << std::endl;
-
-				template_file.close();
-				output_file.close();
-
-				return 1;
-			}
-
 			char next_char;
-			template_file.get(next_char);
+			template_stream.get(next_char);
 
 			if (next_char == '$')
 			{
-				output_file << "$";
+				output_stream << "$";
 			}
 			else if (next_char == '(')
 			{
@@ -68,7 +130,7 @@ int main(int argc, char* argv[])
 				char token_char;
 				bool incomplete = true;
 
-				while (template_file.get(token_char) && !template_file.eof())
+				while (template_stream.get(token_char) && !template_stream.eof())
 				{
 					if (token_char != ')')
 					{
@@ -84,65 +146,49 @@ int main(int argc, char* argv[])
 				if (incomplete)
 				{
 					std::cerr << "Incomplete macro" << std::endl;
-
-					template_file.close();
-					output_file.close();
-
 					return 1;
 				}
 
 				if (!token.empty() && std::isdigit(token[0]))
 				{
-					int arg_index = std::stoi(token) + 3;
+					int arg_index = std::stoi(token);
 
-					if (arg_index <= 2 || arg_index >= argc)
+					if (arg_index < 0 || arg_index >= args.size())
 					{
 						std::cerr << "Invalid macro token: arg index out of range" << std::endl;
-
-						template_file.close();
-						output_file.close();
-
 						return 1;
 					}
 
-					output_file << argv[arg_index];
+					output_stream << args.at(arg_index);
 				}
 				else if (!token.empty() && macros.count(token))
 				{
-					output_file << macros.at(token);
+					output_stream << macros.at(token);
 				}
 				else if (!token.empty())
 				{
-					output_file << std::put_time(&tm, token.c_str());
+					output_stream << std::put_time(time, token.c_str());
 				}
 				else
 				{
 					std::cerr << "Invalid macro token" << std::endl;
-
-					template_file.close();
-					output_file.close();
-
 					return 1;
 				}
 			}
 			else
 			{
 				std::cerr << "Incomplete macro" << std::endl;
-
-				template_file.close();
-				output_file.close();
-
 				return 1;
 			}
 		}
 		else
 		{
-			output_file.put(c);
+			output_stream.put(c);
 		}
 	}
+}
 
-	template_file.close();
-	output_file.close();
+std::filesystem::path reparent_path(const std::filesystem::path& path, const std::filesystem::path& old_root, const std::filesystem::path& new_root)
+{
 
-	return 0;
 }
